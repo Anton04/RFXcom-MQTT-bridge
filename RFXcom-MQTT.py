feda1ff0a07ed5677e66b25ac3386763263021ce
Anton04 <anton.gustafsson@tii.se>
2014-03-30 14:52:02 +0200

    Update RFXcom-MQTT.py
    Test implementation

--- RFXcom-MQTT.py
+++ RFXcom-MQTT.py
@@ -4,7 +4,23 @@ import mosquitto,sys
 transport = PySerialTransport('/dev/ttyUSB0', debug=True)
 transport.reset()
 
+MQTT_HOST = "localhost"
+
+client = mosquitto.Mosquitto("plug-client")
+client.connect(MQTT_HOST)
+
+
+
 while True:
-    event = transport.receive_blocking()
+    event = transport.receive_blocking
+    
+    value = 1
+    
+    client.publish("rfxcom/" + event.device.type_string + "/" + event.device.id_string , value, 1)
     
     print event 
+    
+    
+client.disconnect() 
+
+
