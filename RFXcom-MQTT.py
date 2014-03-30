ca49a9d4b2b06f64b5825a875ad7f8cc7c77f625
Anton04 <anton.gustafsson@tii.se>
2014-03-30 23:18:51 +0200

    Update RFXcom-MQTT.py
    Added outline for controlling stuff

--- RFXcom-MQTT.py
+++ RFXcom-MQTT.py
@@ -7,6 +7,26 @@ LISTEN = True
 PREFIX = "rfxcom"
 MQTT_HOST = "localhost"
 
+def on_connect(mosq, rc):
+    mosq.subscribe(PREFIX+"/#", 0)
+
+def on_message(mosq, msg):
+    print(msg.topic + " " + str(msg.payload))
+    topics = msg.topic.split("/")
+    name = topics[-2]
+    if topics[-1] == "set":
+    value = int(msg.payload)
+    
+    #TODO Create device and set value
+    
+    return value 
+    
+def ControlLoop():
+    # schedule the client loop to handle messages, etc.
+    print "Starting MQTT listener"
+    while True:
+        client.loop()
+        time.sleep(0.1)
 
 transport = PySerialTransport(PORT, debug=True)
 #transport.reset()
@@ -16,7 +36,10 @@ client = mosquitto.Mosquitto("RFXcom-to-MQTT-client")
 #Connect and notify others of our presence. 
 client.connect(MQTT_HOST)
 client.publish("system/RFXcom-to-MQTT", "Online",1)
+client.on_connect = on_connect
+client.on_message = on_message
 
+#Start tread...
 
 while True:
     event = transport.receive_blocking()
